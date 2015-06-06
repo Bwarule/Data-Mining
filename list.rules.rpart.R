30e86c57e67e628bdde9dc87876ac53f456a6e6c
Bwarule <bwarule@gmail.com>
2015-06-06 23:09:22 +0530

    Create list.rules.rpart.R
    R code to extract the rule from decision tree model.

---
+++ list.rules.rpart.R
@@ -0,0 +1,28 @@
+list.rules.rpart <- function(model){
+  if (!inherits(model, "rpart")) stop("Not a legitimate rpart tree")
+  #
+  # Get some information.
+  #
+  frm     <- model$frame
+  names   <- row.names(frm)
+  ylevels <- attr(model, "ylevels")
+  ds.size <- model$frame[1,]$n
+  #
+  # Print each leaf node as a rule.
+  #
+  for (i in 1:nrow(frm))
+  {
+    if (frm[i,1] == "<leaf>")
+    {
+      # The following [,5] is hardwired - needs work!
+      cat("\n")
+      cat(sprintf(" Rule number: %s ", names[i]))
+      cat(sprintf("[yval=%s cover=%d (%.0f%%) prob=%0.2f]\n",
+                  ylevels[frm[i,]$yval], frm[i,]$n,
+                  round(100*frm[i,]$n/ds.size), frm[i,]$yval2[,5]))
+      pth <- path.rpart(model, nodes=as.numeric(names[i]), print.it=FALSE)
+      cat(sprintf("   %s\n", unlist(pth)[-1]), sep="")
+    }
+  }
+}				
+		
